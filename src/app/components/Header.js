813f0fd3c1dc7291ad8bdf0688639f0984579be7
Cloudlabel-2025 <cloudlabel@cloudheard.org>
2025-12-02 14:03:44 +0530

    Nav-section

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,36 +1,47 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
-function Header() {
+export default function Header() {
   const [isClient, setIsClient] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
     setIsClient(true);
+
     if (typeof window !== "undefined" && window.WOW) {
       new window.WOW({ live: false }).init();
     }
   }, []);
 
   if (!isClient) return null;
+  //  Function to close navbar
+  const closeNavbar = () => {
+    const navbar = document.getElementById("navbarCollapse");
+    const bsCollapse = window.bootstrap?.Collapse;
+
+    if (navbar && bsCollapse) {
+      const collapse = bsCollapse.getInstance(navbar);
+      if (collapse) collapse.hide();
+    }
+  };
 
   return (
     <div
-      className="container-fluid fixed-top px-0 wow fadeIn mt-2"
+      className="container-fluid fixed-top px-0 wow fadeIn mt-lg-4 "
       data-wow-delay="0.1s"
     >
-      {/* Optional: Remove empty top bar if not used */}
-      {/* <div className="top-bar row gx-0 align-items-center d-none d-lg-flex"></div> */}
-
       <nav
-        className="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn mt-2"
+        className="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn"
         data-wow-delay="0.1s"
       >
-        <Link className="navbar-brand ms-4 ms-lg-0" href="/">
+        <Link
+          className="navbar-brand ms-4 ms-lg-0"
+          href="/"
+          onClick={closeNavbar}
+        >
           <Image
             src="/img/chc-logo.png"
             alt="Brand Logo"
@@ -39,23 +50,20 @@ function Header() {
             className="d-inline-block align-text-top mt-2"
           />
         </Link>
-
         <button
           type="button"
           className="navbar-toggler me-4"
           data-bs-toggle="collapse"
           data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto p-4 p-lg-0">
+            {/* All Links Close Navbar on Mobile */}
             <Link
               href="/"
+              onClick={closeNavbar}
               className={
                 pathname === "/"
                   ? "nav-item nav-link active"
@@ -64,9 +72,9 @@ function Header() {
             >
               Home
             </Link>
-
             <Link
               href="/about-us"
+              onClick={closeNavbar}
               className={
                 pathname === "/about-us"
                   ? "nav-item nav-link active"
@@ -75,7 +83,7 @@ function Header() {
             >
               About Us
             </Link>
-
+            {/* Expertise Dropdown */}
             <div className="nav-item dropdown">
               <Link
                 href="#"
@@ -88,88 +96,87 @@ function Header() {
                 <Link
                   href="/expertise/cloud-hcm"
                   className={`dropdown-item ${
-                    pathname === "/expertise/cloud-hcm" ? "active" : ""
+                    pathname === "/expertise/cloud-hcm" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Oracle HCM Fusion
                 </Link>
                 <Link
                   href="/expertise/cloud-erp"
                   className={`dropdown-item ${
-                    pathname === "/expertise/cloud-erp" ? "active" : ""
+                    pathname === "/expertise/cloud-erp" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Oracle ERP Fusion
                 </Link>
                 <Link
                   href="/expertise/on-prem-to-cloud"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/on-prem-to-cloud" ? "active" : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/on-prem-to-cloud" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   On Premise to Cloud
                 </Link>
                 <Link
                   href="/expertise/oracle-integration-cloud"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/oracle-integration-cloud"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/oracle-integration-cloud" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Oracle Integration Cloud
                 </Link>
                 <Link
                   href="/expertise/fusion-data-intelligence"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/fusion-data-intelligence"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/fusion-data-intelligence" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Fusion Data Intelligence
                 </Link>
                 <Link
                   href="/expertise/automation-testing"
                   className={`dropdown-item ${
-                    pathname === "/expertise/automation-testing" ? "active" : ""
+                    pathname === "/expertise/automation-testing" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Automation Testing
                 </Link>
                 <Link
                   href="/expertise/version-control-system"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/version-control-system"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/version-control-system" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Version Control System
                 </Link>
                 <Link
                   href="/expertise/application-development"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/application-development"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/application-development" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Application Development
                 </Link>
                 <Link
                   href="/expertise/oracle-subscription"
-                  className={`dropdown-item ${
-                    pathname === "/expertise/oracle-subscription"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/expertise/oracle-subscription" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Oracle Subscription Management
                 </Link>
               </div>
             </div>
-
+            {/* Services Dropdown */}
             <div className="nav-item dropdown">
               <Link
                 href="#"
@@ -181,61 +188,63 @@ function Header() {
               <div className="dropdown-menu border-light m-0">
                 <Link
                   href="/services/managed-services"
-                  className={`dropdown-item ${
-                    pathname === "/services/managed-services" ? "active" : ""
+                   className={`dropdown-item ${
+                    pathname === "/services/managed-services" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Cloud Managed Services
                 </Link>
                 <Link
                   href="/services/implementation-services"
-                  className={`dropdown-item ${
-                    pathname === "/services/implementation-services"
-                      ? "active"
-                      : ""
+                   className={`dropdown-item ${
+                    pathname === "/services/implementation-services" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Cloud Implementation Services
                 </Link>
                 <Link
                   href="/services/integration-services"
                   className={`dropdown-item ${
-                    pathname === "/services/integration-services"
-                      ? "active"
-                      : ""
+                    pathname === "/services/integration-services" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Cloud Integration Services
                 </Link>
                 <Link
                   href="/services/healthcheck-oracle"
-                  className={`dropdown-item ${
-                    pathname === "/services/healthcheck-oracle" ? "active" : ""
+                   className={`dropdown-item ${
+                    pathname === "/services/healthcheck-oracle" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Health Check - Oracle Fusion
                 </Link>
                 <Link
                   href="/services/healthcheck-fdi"
-                  className={`dropdown-item ${
-                    pathname === "/services/healthcheck-fdi" ? "active" : ""
+                   className={`dropdown-item ${
+                    pathname === "/services/healthcheck-fdi" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   FDI - Health Check
                 </Link>
                 <Link
                   href="/services/department-tree"
-                  className={`dropdown-item ${
-                    pathname === "/services/department-tree" ? "active" : ""
+                   className={`dropdown-item ${
+                    pathname === "/services/department-tree" ? "active-dropdown" : ""
                   }`}
+                  onClick={closeNavbar}
                 >
                   Department Tree & Organisation Framework
                 </Link>
               </div>
             </div>
-
             <Link
               href="/careers"
+              onClick={closeNavbar}
               className={
                 pathname === "/careers"
                   ? "nav-item nav-link active"
@@ -246,6 +255,7 @@ function Header() {
             </Link>
             <Link
               href="/contact-us"
+              onClick={closeNavbar}
               className={
                 pathname === "/contact-us"
                   ? "nav-item nav-link active"
@@ -260,5 +270,3 @@ function Header() {
     </div>
   );
 }
-
-export default Header;
